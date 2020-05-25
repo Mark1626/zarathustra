66e7f3e40acd0613f6d1f55dd31d9a48c577936c
Mark1626 <nimalan.m@protonmail.com>
2020-05-25 16:23:32 +0530

    Use fetch package

--- api/linenumber.js
+++ api/linenumber.js
@@ -1,22 +1,21 @@
-const http = require("http");
+const fetch = require("node-fetch");
 
-module.exports = (req, res) => {
+module.exports = (request, response) => {
   const API_URL = `http://api.github.com`;
   const GIST_ID = process.env.GIST_ID;
 
-  http
-    .get(`${API_URL}/gists/${GIST_ID}`, (res) => {
-      const lines = res.files["zarathustra.txt"].content.split("\n")
-        .length;
-      res.status(200).json({
+  fetch(`${API_URL}/gists/${GIST_ID}`)
+    .then((res) => res.json())
+    .then((val) => {
+      const lines = val.files["zarathustra.txt"].content.split("\n").length;
+      response.status(200).json({
         msg: `Hello there are ${lines} lines`,
-        lines
+        lines,
       });
-    })
-    .on("error", (e) => {
-      console.warn(e);
-      res.status(200).json({
-        msg: `Unexpected error occured`,
+    }).catch(err => {
+      response.status(500).json({
+        msg: `Unexpected error occured`
       });
+      console.error(err)
     });
 };
