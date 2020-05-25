e8de5f094330835f48d74e1253cb394f5bb6a1a2
Mark1626 <nimalan.m@protonmail.com>
2020-05-25 20:18:57 +0530

    Update API

--- api/zarathustra.js
+++ api/zarathustra.js
@@ -1,9 +1,49 @@
-module.exports = (req, res) => {
-  const { name } = req.query;
-  const body = req.body;
+const fetch = require("node-fetch");
 
-  res.status(200).json({
-    msg: `Hello ${name}`,
-    body,
+module.exports = (req, res) => {
+  console.log(req.body)
+  const API_KEY = process.env.API_KEY;
+  const API_URL = `https://api.github.com`;
+  const GIST_ID = process.env.GIST_ID;
+  let content;
+
+  fetch(`${API_URL}/gists/${GIST_ID}`)
+    .then((res) => res.json())
+    .then((val) => {
+      content = val.files["zarathustra.txt"].content;
+    })
+    .catch((err) => {
+      response.status(500).json({
+        msg: `Unexpected error occured`,
+      });
+      console.error(err);
+    });
+
+  fetch(
+    `${API_URL}/gists/${GIST_ID}`,
+    {
+      method: "PATCH",
+      headers: {
+        Authorization: `token ${API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        description: "A bot",
+        files: {
+          "zarathustra.txt": {
+            content: "New line 3",
+            filename: "zarathustra.txt",
+          },
+        },
+      }),
+    },
+    (res) => {
+      console.error(res);
+    }
+  ).catch((err) => {
+    // response.status(500).json({
+    //   msg: `Unexpected error occured`,
+    // });
+    console.error(err);
   });
 };
