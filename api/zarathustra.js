960925fe27911675de327983acbf9f6b3e24442a
Mark1626 <nimalan.m@protonmail.com>
2020-05-25 21:17:06 +0530

    Update

--- api/zarathustra.js
+++ api/zarathustra.js
@@ -7,38 +7,36 @@ const GIST_ID = process.env.GIST_ID;
 
 const updateGist = (content) => {
   let resp;
-  fetch(
-    `${API_URL}/gists/${GIST_ID}`,
-    {
-      method: "PATCH",
-      headers: {
-        Authorization: `token ${API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        description: "Thus Spoke Zarathustra",
-        files: {
-          "zarathustra.txt": {
-            content,
-            filename: "zarathustra.txt",
-          },
-        },
-      }),
+  fetch(`${API_URL}/gists/${GIST_ID}`, {
+    method: "PATCH",
+    headers: {
+      Authorization: `token ${API_KEY}`,
+      "Content-Type": "application/json",
     },
-    (res) => {
+    body: JSON.stringify({
+      description: "Thus Spoke Zarathustra",
+      files: {
+        "zarathustra.txt": {
+          content,
+          filename: "zarathustra.txt",
+        },
+      },
+    }),
+  })
+    .then((res) => {
       console.log("Gist updated succesfully");
       resp = {
         status: 200,
         msg: `Log added`,
       };
-    }
-  ).catch((err) => {
-    resp = {
-      status: 500,
-      msg: `Unexpected error occured`,
-    };
-    console.error(err);
-  });
+    })
+    .catch((err) => {
+      resp = {
+        status: 500,
+        msg: `Unexpected error occured`,
+      };
+      console.error(err);
+    });
   return resp;
 };
 
